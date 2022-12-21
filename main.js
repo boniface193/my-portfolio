907c20eee09e1d0a758f7145f5bf42022db40d2a
Benard Asiimwe Bataringaya <benard.asiimwe.bataringaya@gmail.com>
2022-12-21 11:05:59 +0300

    add listener to navigation

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const navLink = document.querySelector('#navLink');
 const sideBar = document.querySelector('#sideBar');
+const navLinkLi = navLink.querySelectorAll(':scope > li');
 
 function openBar() {
   navLink.classList.toggle('d-none');
   sideBar.classList.toggle('d-none');
 }
+
+navLinkLi.forEach(i => {
+  i.addEventListener('click', function() {
+    openBar()
+  })
+});
