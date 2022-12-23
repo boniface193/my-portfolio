33f6017e2cd5361e47eb0ed21a59aa2d578dd14f
deboik193 <deboik193@gmail.com>
2022-12-23 13:33:30 +0100

    form validation included in the js file

--- js/main.js
+++ js/main.js
@@ -5,33 +5,48 @@ import sidebar from './sidebar.js';
 sidebar();
 
 const portfolioProjects = document.querySelector('#Portfolio');
+const formValidate = document.querySelector('#validateForm')
 
-projectInfo.forEach((i, index) => {
+projectInfo.forEach((element, index) => {
   portfolioProjects.firstElementChild.innerHTML += `<article class="d-flex card">
-        <img src="${i.image}" alt="${index + 1} project image">
+        <img src="${element.image}" alt="${index + 1} project image">
 
         <aside class="mt-12">
             <div class="weight--700 card-sub-title">
-                ${i.title}
+                ${element.title}
             </div>
             <ul class="list-style-type-none d-flex weight--600--500 canopy">
-                <li class="canopy-bold">${i.project.projectTitle}</li>
+                <li class="canopy-bold">${element.project.projectTitle}</li>
                 <li class="dot"></li>
-                <li class="canopy-not-bold">${i.project.stack}</li>
+                <li class="canopy-not-bold">${element.project.stack}</li>
                 <li class="dot"></li>
-                <li>${i.project.yearCompleted}</li>
+                <li>${element.project.yearCompleted}</li>
             </ul>
-            <p class="card-title weight--400">${i.description.short}</p>
+            <p class="card-title weight--400">${element.description.short}</p>
             <ul class="list-style-type-none d-flex lang padding-every-where weight--500">
-            ${i.language.map((l) => `<li>${l}</li>`).join('')}
+            ${element.language.map((l) => `<li>${l}</li>`).join('')}
             </ul>
-            ${
-  i.buttons.seeProject
-    ? '<button type="button" class="btn weight--500">See Project</button>'
-    : ''
-}
+            ${element.buttons.seeProject
+      ? '<button type="button" class="btn weight--500">See Project</button>'
+      : ''
+    }
       </aside>
     </article>`;
 });
 
 getSingleProject();
+
+
+formValidate.addEventListener('submit', function (params) {
+  const allInput = Array.from(formValidate.querySelectorAll('input'))
+  const isLowerCase = allInput[1]
+
+  if (isLowerCase.value != isLowerCase.value.toLowerCase()) {
+    const errorMsg = document.querySelector('.error')
+    errorMsg.innerText = 'Please make sure your email address is in lower case'
+    errorMsg.classList.toggle('d-none')
+
+    params.preventDefault()
+    return false
+  }
+})
